c889d1ec131e9d2031df8f40e8803a5215a59989
Mythia <104918114+Xyren-1140@users.noreply.github.com>
2022-05-08 04:35:46 +0800

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -10,53 +10,29 @@ from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
 
-@Bot.on_message(
-    filters.private
-    & filters.user(ADMINS)
-    & ~filters.command(
-        ["start", "users", "broadcast", "ping", "batch", "genlink", "promote",  "admins",  "promote",  "help",  "logs"]
-    )
-)
+@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("<code>Sedang Memproses Mohon Tunggu...</code>", quote=True)
+    reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
-        post_message = await message.copy(
-            chat_id=client.db_channel.id, disable_notification=True
-        )
+        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except FloodWait as e:
         await asyncio.sleep(e.x)
-        post_message = await message.copy(
-            chat_id=client.db_channel.id, disable_notification=True
-        )
+        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
-        await reply_text.edit_text("<b>Telah Terjadi suatu kesalahan silakan cek /logs apa yang sedang terjadi...</b>")
+        await reply_text.edit_text("Something went Wrong..!")
         return
     converted_id = post_message.message_id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "🔁 Bagikan Link", url=f"https://telegram.me/share/url?url={link}"
-                )
-            ]
-        ]
-    )
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 
-    await reply_text.edit(
-        f"<b>Link Sharing File Berhasil Di Buat :</b>\n\n{link}",
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-    )
+    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
-
-
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited)
 async def new_post(client: Client, message: Message):
@@ -68,16 +44,9 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
-                )
-            ]
-        ]
-    )
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
         print(e)
+        pass
